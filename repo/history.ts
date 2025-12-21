2c2d892e6cb93dfa9c787f34ebb5d1a9d1ef5587
Georg Graf (SPG) <grafg@spengergasse.at>
2025-12-21 07:55:01 +0100

    keep the nice select

--- repo/history.ts
+++ repo/history.ts
@@ -1,7 +1,10 @@
 import { db } from "./db.ts";
 
 const select_event_stmt = db.prepare(
-  "select * from history where at between (select strftime('%Y-%m-%dT%H:%M:%fZ',datetime(?, 'utc'))) and (select strftime('%Y-%m-%dT%H:%M:%fZ',datetime(?, 'utc'))) order by at",
+  `select * from history 
+  where at 
+  between (select strftime('%Y-%m-%dT%H:%M:%fZ',datetime(?, 'utc'))) 
+  and (select strftime('%Y-%m-%dT%H:%M:%fZ',datetime(?, 'utc'))) order by at`,
 );
 
 export function selectHistoryRange(start: string, end: string) {
