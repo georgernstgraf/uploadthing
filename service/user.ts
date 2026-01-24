5789c5c47c8d0131ec30f10bbe4ab83410f22434
Georg Graf (SPG) <grafg@spengergasse.at>
2026-01-24 14:17:13 +0100

    var rename

--- service/user.ts
+++ service/user.ts
@@ -1,25 +1,25 @@
-import * as user from "../repo/user.ts";
+import * as userRepo from "../repo/user.ts";
 import { UserType } from "../lib/types.ts";
 
 export function ofIPs(ips: string[]): Map<string, UserType> {
     const result = new Map<string, UserType>();
-    const users = user.searchbyips(ips);
+    const users = userRepo.searchbyips(ips);
     for (const u of users) {
         result.set(u.ip!, u);
     }
     return result;
 }
 export function getbyip(ip: string): UserType | null {
-    const result = user.searchbyip(ip);
+    const result = userRepo.searchbyip(ip);
     if (!result) return null;
     return result as UserType;
 }
 export function register(userData: UserType) {
-    user.registerip(userData);
+    userRepo.registerip(userData);
 }
 
 export function get_registered_ips(ips: string[]): Set<string> {
-    const users = user.searchbyips(ips);
+    const users = userRepo.searchbyips(ips);
     const registered_ips = new Set<string>();
     for (const u of users) {
         if (u.ip) {
