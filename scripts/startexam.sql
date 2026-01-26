7a7b221d82c049a0d18bcd437a7141f4844c7ba8
Georg Graf (SPG) <grafg@spengergasse.at>
2026-01-26 02:20:48 +0100

    make startexam.sql a shebang-style bash script

--- scripts/startexam.sql
+++ scripts/startexam.sql
@@ -1,6 +1,9 @@
-WITH moved_rows AS (
+#!/usr/bin/env bash
+cd || exit
+
+echo 'WITH moved_rows AS (
     DELETE FROM registrations
     RETURNING *
 )
 INSERT INTO forensic_registrations
-SELECT * FROM moved_rows;
+SELECT * FROM moved_rows;' | sqlite3 ../uploadthing.db
