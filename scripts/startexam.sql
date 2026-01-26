de4d16151ac5f9b6b9170378eca39a767728f41c
Georg Graf (SPG) <grafg@spengergasse.at>
2026-01-26 02:45:15 +0100

    acp-commit

--- scripts/startexam.sql
+++ scripts/startexam.sql
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 cd || exit
-set -v
-echo '
-BEGIN TRANSACTION;
-
+echo 'BEGIN TRANSACTION;
 INSERT INTO forensic_registrations SELECT * FROM registrations;
 DELETE FROM registrations;
-
 COMMIT;' \
-| sqlite3 ~/uploadthing/uploadthing.db
+| tee /dev/tty | sqlite3 ~/uploadthing/uploadthing.db
