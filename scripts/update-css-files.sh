ca370f2b3a8192e9bd4af210c403fb4052583a79
Georg Graf (SPG) <grafg@spengergasse.at>
2026-02-14 21:37:34 +0100

    Fix contrast issues in all themes, add leisure-* variables (issue #71)

---
+++ scripts/update-css-files.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+scripts/update-css-colors.sh static/lego-darkblue-theme.css static/img/bg-lego-darkblue.png 
+scripts/update-css-colors.sh static/lego-lightblue-theme.css static/img/bg-lego-lightblue.png 
+scripts/update-css-colors.sh static/leisure-pink-theme.css static/img/bg-leisure-pink.png 
+scripts/update-css-colors.sh static/leisure-purple-theme.css static/img/bg-leisure-purple.png 
