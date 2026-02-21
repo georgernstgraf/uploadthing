feef99f1e84d67b2bad6e47c9e6fe852abf16a2f
Georg Graf (SPG) <grafg@spengergasse.at>
2026-02-21 20:47:26 +0100

    better test infra

--- main_test_db.ts
+++
@@ -1,5 +0,0 @@
-// import config from "./config.ts";
-import db from "./repo/db.ts";
-
-// Close the databases
-db.close();
---
+++ scripts/test-with-server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -veuo pipefail
+source .env
+
+# set up a running dev server
+fuser -k ${LISTEN_PORT}/tcp && (echo "Killed existing process on port ${LISTEN_PORT}, waiting .."; sleep 1)
+fuser -k -9 ${LISTEN_PORT}/tcp && (echo "Killed -9 existing process on port ${LISTEN_PORT}, waiting .."; sleep .3)
+deno task start > /tmp/${LISTEN_HOST}.log 2>&1 &
+echo "waiting, log is in /tmp/${LISTEN_HOST}.log"
+sleep 2
+
+# run the tests to stdout
+deno test -A
+TEST_EXIT_CODE=$?
+
+# kill the dev server again
+fuser -k ${LISTEN_PORT}/tcp && (echo "Killed existing process on port ${LISTEN_PORT}, waiting .."; sleep 1)
+fuser -k -9 ${LISTEN_PORT}/tcp && echo "Killed -9 existing process on port ${LISTEN_PORT}, waiting .."
+exit $TEST_EXIT_CODE
