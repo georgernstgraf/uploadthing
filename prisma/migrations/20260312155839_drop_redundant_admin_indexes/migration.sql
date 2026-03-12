9dd6e367709b18bf8e52199ca659cb6d923fc5fc
Georg Graf (SPG) <grafg@spengergasse.at>
2026-03-12 17:00:18 +0100

    perf: drop redundant admin indexes (#102)

---
+++ prisma/migrations/20260312155839_drop_redundant_admin_indexes/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "cookiepresents_ip_idx";
+
+-- DropIndex
+DROP INDEX "ipfact_seen_idx";
+
+-- DropIndex
+DROP INDEX "ipfact_ip_idx";
+
+-- DropIndex
+DROP INDEX "registrations_ip_idx";
